f96a011c3e845e024fd45766a84ff7df9338edd3
wedv <770977802@qq.com>
2014-11-11 17:19:23 +0800

    Update jss.js

--- jss.js
+++ jss.js
@@ -1,5 +1,13 @@
 function aaa(a){
     alert('aaa('+a+')'+typeof(jQuery)+typeof($));
-    jQuery('#lxb-buy-hide-transferId').val(a);
-    jQuery('#lxb-buy-hide-submit').click();
+    jQuery.ajax({
+        url:'http://www.renrendai.com/transfer/loanTransferDetail.action',
+        data:{transferId: a},
+        dataType:'html',
+        success:function(d){
+            var re = new RegExp("最大可购买份数 (\d+) 份",["i"]);
+            var res = d.match(re);
+            alert(res);
+        }
+    });
 }
