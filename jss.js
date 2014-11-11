119c8558ce3f05d2e9fc723445ded82efe1db07b
wedv <770977802@qq.com>
2014-11-11 18:25:49 +0800

    Update jss.js

--- jss.js
+++ jss.js
@@ -15,6 +15,16 @@ function aaa(a){
                 $input = $input.replace('hidden', 'text');
                 jQuery('#lxb-buy-hide').append(jQuery($input));
             }
+            jQuery('#lxb-buy-hide').append('<div id="lxb-buy-captch"></div>');
+            jQuery('#lxb-buy-hide').append('<div id="lxb-buy-captch-button"><button onclick="getCaptch"></button></div>');
+            getCaptch();
         }
     });
+}
+
+function getCaptch(){
+    var d = new Date;
+    var t = d.getTime();
+    var url = "http://www.renrendai.com/image_https.jsp?_=" + t;
+    jQuery('#lxb-buy-captch').html('<img src="' + url + '">');
 }
