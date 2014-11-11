3e4b5b371d3d671b1babf2aebd275f8e94cf62ee
wedv <770977802@qq.com>
2014-11-12 00:05:19 +0800

    rrd

--- jss.js
+++ jss.js
@@ -1,5 +1,5 @@
 function aaa(a){
-    alert('aaa('+a+')'+typeof(jQuery)+typeof($));
+    //alert('aaa('+a+')'+typeof(jQuery)+typeof($));
     jQuery('#lxb-buy-hide').html('');
     jQuery.ajax({
         url:'http://www.renrendai.com/transfer/loanTransferDetail.action',
