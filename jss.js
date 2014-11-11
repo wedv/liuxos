8b80866244a7dbeeef992cd0a9786237ea523db8
wedv <770977802@qq.com>
2014-11-11 17:46:11 +0800

    Update jss.js

--- jss.js
+++ jss.js
@@ -5,7 +5,7 @@ function aaa(a){
         data:{transferId: a},
         dataType:'text',
         success:function(d){
-            var re = /(<a [^>]/i;
+            var re = new RegExp('<a [^>]/i', 'i');
             var res = d.match(re);
             console.log(res);
         }
