c86c21b8ad0910c7a57d457bdae25983a10f0dbf
770977802@qq.com <770977802@qq.com>
2014-11-18 09:27:44 +0800

    ocrking

--- jss.js
+++ jss.js
@@ -74,3 +74,15 @@ function renderUserInfo(){
         }
     });
 }
+function setCookie(name, value) {
+    var Days = 30; //此 cookie 将被保存 30 天
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+function getCookie(name) {
+    var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+    if (arr != null)
+        return unescape(arr[2]);
+    return null;
+}
