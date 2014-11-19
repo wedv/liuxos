a2448ba5ae4e5f73ed4af483c92f75926d5e1be6
770977802@qq.com <770977802@qq.com>
2014-11-19 19:58:05 +0800

    interface

--- rrd_helper.user.js
+++ rrd_helper.user.js
@@ -11,7 +11,7 @@
 // @require		http://www.renrendai.com/static/js/lib/jquery/1.9.1/jquery.js
 // @icon		http://www.renrendai.com/favicon.ico
 // @run-at		document-idle
-// @version 		1013
+// @version 		1020
 // @updateURL		http://wedv.github.io/liuxos/rrd_helper.user.js
 // @supportURL		https://github.com/wedv/php-tool
 // @homepage		https://github.com/wedv/php-tool
