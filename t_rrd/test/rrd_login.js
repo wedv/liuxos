8e9409af327c5eb7e2a114986128c589e49b2f60
wedv <770977802@qq.com>
2015-05-10 12:31:28 +0800

    Update rrd_login.js

--- t_rrd/test/rrd_login.js
+++ t_rrd/test/rrd_login.js
@@ -2,6 +2,7 @@ var ext_j_password='W6pPhRwPRPV1aqfFSEJtHonJELRYB9d3duNUbWKkZMDRj11kjB9Hvtsxe/du
 function ext_login(){
 	jQuery.ajax({
 		url:'https://www.renrendai.com/j_spring_security_check',
+		type:'post',
 		data:{j_username:'770977802@qq.com',j_password:ext_j_password,rememberme:'on',targetUrl:'http://www.renrendai.com/',returnUrl:''}
 	});
 }
