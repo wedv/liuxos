feec1088a135e897484a20493eb560022b277b87
wedv <770977802@qq.com>
2015-05-10 12:28:48 +0800

    Update rrd_login.js

--- t_rrd/test/rrd_login.js
+++ t_rrd/test/rrd_login.js
@@ -1,7 +1,7 @@
 var ext_j_password='W6pPhRwPRPV1aqfFSEJtHonJELRYB9d3duNUbWKkZMDRj11kjB9Hvtsxe/du73Govao2sQJuKrYl/Pfcmq70KhvA7nnbLmbSeEnH6yWoXeLgvs3Z/+tYzLutLMYCZVBscApK+BFxLRSi9SHExYicPu8dWz69ayHAjcVGow+trsI=';
 function ext_login(){
 	jQuery.ajax({
-		url:'http://www.renrendai.com/j_spring_security_check',
+		url:'https://www.renrendai.com/j_spring_security_check',
 		data:{j_username:'770977802@qq.com',j_password:ext_j_password,rememberme:'on',targetUrl:'http://www.renrendai.com/',returnUrl:''}
 	});
 }
