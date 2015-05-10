e9e618d2a8807fc0e821df11bf4bf8b44368071e
wedv <770977802@qq.com>
2015-05-10 12:41:40 +0800

    Update rrd_login.js

--- t_rrd/test/rrd_login.js
+++ t_rrd/test/rrd_login.js
@@ -3,6 +3,9 @@ function ext_login(){
 	jQuery.ajax({
 		url:'https://www.renrendai.com/j_spring_security_check',
 		type:'post',
-		data:{j_username:'770977802@qq.com',j_password:ext_j_password,rememberme:'on',targetUrl:'http://www.renrendai.com/',returnUrl:''}
+		data:{j_username:'770977802@qq.com',j_password:ext_j_password,rememberme:'on',targetUrl:'http://www.renrendai.com/',returnUrl:''},
+		success:function(data, textStatus){
+			this; // 调用本次AJAX请求时传递的options参数
+		}
 	});
 }
