1d038caa2d0faee944d7fb9171d94a9b8e29fd7e
wedv <770977802@qq.com>
2015-08-20 19:19:58 +0800

    Update jss.js

--- t_rrd_new/jss.js
+++ t_rrd_new/jss.js
@@ -138,9 +138,7 @@ function setCookie(name, value) {
     exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
     document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
 }
-var exp = new Date();
-exp.setTime(exp.getTime());
-setCookie('rrd_page_init_time', exp.toGMTString());
+
 function getCookie(name) {
     var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
     if (arr != null)
